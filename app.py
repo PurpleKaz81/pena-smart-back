6b73ee672b5a61d785d2f30a3c7f3a97d37d0d0c
rafael kasinski <rakasin@gmail.com>
2023-06-29 14:01:29 -0300

    Fixed sys print, imported qualifiers from utilities, fixed app.route

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from utilities import qualifiers
 import sys
-print(sys.executable)
 
 app = Flask(__name__)
 CORS(app)
@@ -11,20 +11,10 @@ def hello_world():
     return jsonify(message='Hello, World!')
 
 @app.route('/api/qualifiers', methods=['GET'])
-def get_qualifiers():
-    qualifiers = [
-        { "id": "qualifier-1", "label": "inciso I", "value": "mediante paga ou promessa de recompensa, ou por outro motivo torpe", "years": 0 },
-        { "id": "qualifier-2", "label": "inciso II", "value": "por motivo fútil", "years": 0 },
-        { "id": "qualifier-3", "label": "inciso III", "value": "com emprego de veneno, fogo, explosivo, asfixia, tortura ou outro meio insidioso ou cruel, ou de que possa resultar perigo comum", "years": 0 },
-        { "id": "qualifier-4", "label": "inciso IV", "value": "à traição, de emboscada, ou mediante dissimulação ou outro recurso que dificulte ou torne impossível a defesa do ofendido", "years": 0 },
-        { "id": "qualifier-5", "label": "inciso V", "value": "para assegurar a execução, a ocultação, a impunidade ou vantagem de outro crime", "years": 0 },
-        { "id": "qualifier-6", "label": "inciso VI", "value": "contra a mulher por razões da condição de sexo feminino", "years": 0 },
-        { "id": "qualifier-7", "label": "inciso VII", "value": "contra autoridade ou agente descrito nos arts. 142 e 144 da Constituição Federal, integrantes do sistema prisional e da Força Nacional de Segurança Pública, no exercício da função ou em decorrência dela, ou contra seu cônjuge, companheiro ou parente consanguíneo até terceiro grau, em razão dessa condição", "years": 0 },
-        { "id": "qualifier-8", "label": "inciso VIII", "value": "com emprego de arma de fogo de uso restrito ou proibido", "years": 0 },
-        { "id": "qualifier-9", "label": "inciso IX", "value": "contra menor de 14 (quatorze) anos", "years": 0 },
-    ]
+def get_all_qualifiers():
+    return qualifiers.get_qualifiers()
 
-    return jsonify(qualifiers)
 
 if __name__ == '__main__':
+    print(sys.executable)
     app.run(debug=True)
